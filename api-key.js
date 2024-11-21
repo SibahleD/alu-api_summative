47798c746b2817f05dee36f63e23a7730b6f6c6a
SibahleD <s.dlamini2@alustudent.com>
2024-11-21 11:26:55 +0200

    jin

---
+++ api-key.js
@@ -0,0 +1,1 @@
+let apiKey = "577a3092146f49e2bcd2c5d8b68d323a";
--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 // API KEY : 577a3092146f49e2bcd2c5d8b68d323a
+//API Used: https://newsapi.org/v2/top-headlines?country=us&apiKey=577a3092146f49e2bcd2c5d8b68d323a
 
+// Hamurger code for header
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".index-menu");
 
@@ -12,3 +14,78 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+// // API WORK
+// //API Used: http://newsapi.org/s/india-news-api
+// const container = document.querySelector(".container");
+// const optionsContainer = document.querySelector(".options-container");
+// // "in" stands for India
+// const country = "in";
+// const options = [
+//   "general",
+//   "entertainment",
+//   "health",
+//   "science",
+//   "sports",
+//   "technology",
+// ];
+// //100 requests per day
+// let requestURL;
+// //Create cards from data
+// const generateUI = (articles) => {
+//   for (let item of articles) {
+//     let card = document.createElement("div");
+//     card.classList.add("news-card");
+//     card.innerHTML = `<div class="news-image-container">
+//     <img src="${item.urlToImage || "./newspaper.jpg"}" alt="" />
+//     </div>
+//     <div class="news-content">
+//       <div class="news-title">
+//         ${item.title}
+//       </div>
+//       <div class="news-description">
+//       ${item.description || item.content || ""}
+//       </div>
+//       <a href="${item.url}" target="_blank" class="view-button">Read More</a>
+//     </div>`;
+//     container.appendChild(card);
+//   }
+// };
+// //News API Call
+// const getNews = async () => {
+//   container.innerHTML = "";
+//   let response = await fetch(requestURL);
+//   if (!response.ok) {
+//     alert("Data unavailable at the moment. Please try again later");
+//     return false;
+//   }
+//   let data = await response.json();
+//   generateUI(data.articles);
+// };
+// //Category Selection
+// const selectCategory = (e, category) => {
+//   let options = document.querySelectorAll(".option");
+//   options.forEach((element) => {
+//     element.classList.remove("active");
+//   });
+//   requestURL = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`;
+//   e.target.classList.add("active");
+//   getNews();
+// };
+// //Options Buttons
+// const createOptions = () => {
+//   for (let i of options) {
+//     optionsContainer.innerHTML += `<button class="option ${
+//       i == "general" ? "active" : ""
+//     }" onclick="selectCategory(event,'${i}')">${i}</button>`;
+//   }
+// };
+// const init = () => {
+//   optionsContainer.innerHTML = "";
+//   getNews();
+//   createOptions();
+// };
+// window.onload = () => {
+//   requestURL = `https://newsapi.org/v2/top-headlines?country=${country}&category=general&apiKey=${apiKey}`;
+//   init();
+// };
